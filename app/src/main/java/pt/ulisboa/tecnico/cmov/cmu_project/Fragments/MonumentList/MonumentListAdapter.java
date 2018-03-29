654756c96d5b09f787dfa023f9e344453cc5fe7e
Tiago Monteiro <tiagoasmont@gmail.com>
2018-03-29 01:55:31 +0100

    fixes and ajustments

--- app/src/main/java/pt/ulisboa/tecnico/cmov/cmu_project/Fragments/MonumentList/MonumentListAdapter.java
+++ app/src/main/java/pt/ulisboa/tecnico/cmov/cmu_project/Fragments/MonumentList/MonumentListAdapter.java
@@ -1,11 +1,16 @@
 package pt.ulisboa.tecnico.cmov.cmu_project.Fragments.MonumentList;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.support.v7.widget.CardView;
+import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -27,39 +32,79 @@ public class MonumentListAdapter extends ArrayAdapter<Monument> {
     }
 
     @Override
+    public int getViewTypeCount() {
+        return 2;
+    }
+
+    @Override
+    public int getItemViewType(int pos) {
+
+        return monuments.get(pos) == null ? 1 : 0;
+    }
+
+    @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        if (convertView == null) {
-            convertView = LayoutInflater.from(getContext()).inflate(R.layout.monument_list_item, parent, false);
-        }
 
 
         Monument monument = monuments.get(position);
 
+
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(getItemViewType(position) == 1 ?
+                    R.layout.monument_list_start : R.layout.monument_list_item, parent, false);
+        }
+
+        if (monument == null) {
+
+            TextView textView = convertView.findViewById(R.id.startMonumentListText);
+            LinearLayout linearLayout = convertView.findViewById(R.id.startLinear);
+
+            if (position == 0) {
+                textView.setText("START");
+                linearLayout.setPadding(convertDpToPixel(5), convertDpToPixel(5), 0, 0);
+                textView.setBackgroundColor(getContext().getResources().getColor(R.color.colorQuizPath));
+            } else {
+                textView.setText("END");
+                Monument prevMonument = monuments.get(position - 1);
+                linearLayout.setPadding(convertDpToPixel(5), 0, 0, convertDpToPixel(5));
+                Log.i("COLOR", (prevMonument != null && !prevMonument.getStatus().equals(Monument.VISITED)) + "");
+                Log.i("COLOR", (prevMonument != null) + "");
+                Log.i("COLOR", (!(prevMonument.getStatus().equals(Monument.VISITED))) + "");
+                textView.setBackgroundColor(getContext().getResources().getColor(prevMonument != null && !prevMonument.getStatus().equals(Monument.VISITED) ?
+                        R.color.colorGray : R.color.colorQuizPath));
+            }
+
+            return convertView;
+        }
+
+
         TextView status = convertView.findViewById(R.id.status);
+
         status.setText(monument.getStatus().toUpperCase());
         status.setBackgroundColor(getContext().getResources().getColor(getColor(monument.getStatus())));
 
         View viewBefore = convertView.findViewById(R.id.viewBefore);
         if (monument.getStatus().equals(Monument.VISITED) || monument.getStatus().equals(Monument.QUIZ)) {
             viewBefore.setBackgroundColor(getContext().getResources().getColor(R.color.colorQuizPath));
-        }else {
+        } else {
             viewBefore.setBackgroundColor(getContext().getResources().getColor(R.color.colorLightGray));
         }
 
         View viewAfter = convertView.findViewById(R.id.viewAfter);
         if (monument.getStatus().equals(Monument.VISITED)) {
             viewAfter.setBackgroundColor(getContext().getResources().getColor(R.color.colorQuizPath));
-        }else {
+        } else {
             viewAfter.setBackgroundColor(getContext().getResources().getColor(R.color.colorLightGray));
         }
 
         CardView statusCardView = convertView.findViewById(R.id.statusCardView);
-        if (monument.getStatus().equals(Monument.VISITED) || monument.getStatus().equals(Monument.QUIZ)) {
+        if (monument.getStatus().equals(Monument.VISITED)) {
             statusCardView.setCardBackgroundColor(getContext().getResources().getColor(R.color.colorQuizPath));
-        }else {
+        } else if (monument.getStatus().equals(Monument.QUIZ)) {
+            statusCardView.setCardBackgroundColor(getContext().getResources().getColor(R.color.colorQuiz));
+        } else {
             statusCardView.setCardBackgroundColor(getContext().getResources().getColor(R.color.colorGray));
         }
-
 
         return convertView;
     }
@@ -79,5 +124,11 @@ public class MonumentListAdapter extends ArrayAdapter<Monument> {
                 return R.color.colorVisited;
         }
 
+    }
+
+    public int convertDpToPixel(float dp) {
+        DisplayMetrics metrics = Resources.getSystem().getDisplayMetrics();
+        float px = dp * (metrics.densityDpi / 160f);
+        return Math.round(px);
     }
 }
