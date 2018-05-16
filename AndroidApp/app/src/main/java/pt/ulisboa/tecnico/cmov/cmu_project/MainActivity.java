79a08b521ec713a76131d9a02ac3f13eef6fa74d
Nuno Santos <nuno_913@hotmail.com>
2018-05-16 11:55:31 +0100

    Solved bug with manifest merge

--- AndroidApp/app/src/main/java/pt/ulisboa/tecnico/cmov/cmu_project/MainActivity.java
+++ AndroidApp/app/src/main/java/pt/ulisboa/tecnico/cmov/cmu_project/MainActivity.java
@@ -87,10 +87,10 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-        if (userName != null) {
-            TextView usernameText = findViewById(R.id.usernameFragmentMain);
+        /*if (userName != null) {
+            TextView usernameText = findViewById(R.id.username);
             usernameText.setText(userName);
-        }
+        }*/
 
         if (DatabaseHelper.getInstance(getBaseContext()).tableIsEmpty(DatabaseHelper.TABLE_MONUMENTS))
             getMonuments();
